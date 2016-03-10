8d4f56ce364c201ce1a909fc9353215ec9783dba
Russell Greene <russellgreene8@gmail.com>
2016-03-09 21:32:33 -0700

    Update doxygen documentation

--- search/functions_1.js
+++ search/functions_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['getcomponentid',['getComponentID',['../structManager.html#a6e501a959af6552e2521f2022ab4efdd',1,'Manager']]],
   ['getmycomponentid',['getMyComponentID',['../structManager.html#ae2b0321410fbd8b40b3056bfab20c4a1',1,'Manager']]],
-  ['getmystoragecomponentid',['getMyStorageComponentID',['../structManager.html#a04d32b9b444f925dba8ec64fc4edc42a',1,'Manager']]]
+  ['getmystoragecomponentid',['getMyStorageComponentID',['../structManager.html#a04d32b9b444f925dba8ec64fc4edc42a',1,'Manager']]],
+  ['getstoragecomponentid',['getStorageComponentID',['../structManager.html#a9956b99e3b94d7d6e410c0c8064a4e67',1,'Manager']]]
 ];
