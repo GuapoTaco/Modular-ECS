66e466afb99d67b51046307f53865660593be5ad
Russell Greene <russellgreene8@gmail.com>
2016-03-09 21:24:14 -0700

    Use cp again, but smartly

--- update-docs.sh
+++ update-docs.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -7,6 +7,7 @@ cd $DIR
 # make sure we aren't on the gh-pages branch
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
 branch_name="(unnamed branch)"     # detached HEAD
+
 
 if [ "$branch_name" == "gh-pages" ]
 then 
@@ -31,11 +32,8 @@ cp -r $DIR/build/doc $TMP_DIR/doc
 # checkout gh-pages
 git checkout gh-pages
 
-# remove the old docs, and not hidden files
-rm -rf $(ls)
-
 # copy them over
-rsync -abviuzP $TMP_DIR/doc/html $DIR/
+cp -R $TMP_DIR/doc/html/* $DIR
 
 # delete the tempory directory
 rm -r $TMP_DIR
